43f9f0ea00786d94c9a1573cc9e7a983ea691c07
Sribarathvajasarma <gsribarathvajasarma@gmail.com>
2023-02-23 23:05:36 -0800

    Change main.go 3

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "This is dfdsfsdfd")
+		fmt.Fprintf(w, "This is dfds")
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
